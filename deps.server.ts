08a3c408765989fd3f093385b11de75f9a584c80
navigaid <navigaid@gmail.com>
2022-11-22 20:28:19 +0800

    bump esbuild-wasm to 0.15.15

--- deps.server.ts
+++ deps.server.ts
@@ -2,5 +2,5 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 // esbuild
-export * as esbuild from "https://esm.sh/esbuild-wasm@0.15.9";
+export * as esbuild from "https://esm.sh/esbuild-wasm@0.15.15";
 export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts";
